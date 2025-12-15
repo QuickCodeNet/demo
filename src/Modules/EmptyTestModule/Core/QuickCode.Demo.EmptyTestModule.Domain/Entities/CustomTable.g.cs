c3aa98f10646290332537895f48ba7044a8d9ed5
QuickCode Auto Commit <uzeyirapaydin@gmail.com>
2025-12-15 09:44:07 -0500

    QuickCode.Net Auto Commit: 12/15/2025 2:44:07 PM

---
+++ src/Modules/EmptyTestModule/Core/QuickCode.Demo.EmptyTestModule.Domain/Entities/CustomTable.g.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using QuickCode.Demo.EmptyTestModule.Domain;
+using QuickCode.Demo.Common;
+using QuickCode.Demo.Common.Auditing;
+
+namespace QuickCode.Demo.EmptyTestModule.Domain.Entities;
+
+[Table("CUSTOM_TABLES")]
+public partial class CustomTable : BaseSoftDeletable, IAuditableEntity 
+{
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	[Column("ID")]
+	public int Id { get; set; }
+	
+	[Column("NAME")]
+	[StringLength(250)]
+	public string Name { get; set; }
+	
+	[Column("SURNAME")]
+	[StringLength(250)]
+	public string Surname { get; set; }
+	
+	[Column("JSON_DATA")]
+	[StringLength(int.MaxValue)]
+	public string JsonData { get; set; }
+	}
+
---
+++ src/Modules/EmptyTestModule/Presentation/QuickCode.Demo.EmptyTestModule.Api/Controllers/CustomTablesController.g.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using QuickCode.Demo.Common.Controllers;
+using QuickCode.Demo.EmptyTestModule.Application.Dtos.CustomTable;
+using QuickCode.Demo.EmptyTestModule.Application.Services.CustomTable;
+
+namespace QuickCode.Demo.EmptyTestModule.Api.Controllers
+{
+    public partial class CustomTablesController : QuickCodeBaseApiController
+    {
+        private readonly ICustomTableService service;
+        private readonly ILogger<CustomTablesController> logger;
+        private readonly IServiceProvider serviceProvider;
+        public CustomTablesController(ICustomTableService service, IServiceProvider serviceProvider, ILogger<CustomTablesController> logger)
+        {
+            this.service = service;
+            this.logger = logger;
+            this.serviceProvider = serviceProvider;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<CustomTableDto>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        public async Task<IActionResult> ListAsync([FromQuery] int? page, int? size)
+        {
+            if (ValidatePagination(page, size) is {} error)
+                return error;
+            var response = await service.ListAsync(page, size);
+            if (HandleResponseError(response, logger, "CustomTable", "List") is {} responseError)
+                return responseError;
+            return Ok(response.Value);
+        }
+
+        [HttpGet("count")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        public async Task<IActionResult> CountAsync()
+        {
+            var response = await service.TotalItemCountAsync();
+            if (HandleResponseError(response, logger, "CustomTable") is {} responseError)
+                return responseError;
+            return Ok(response.Value);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CustomTableDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        public async Task<IActionResult> GetItemAsync(int id)
+        {
+            var response = await service.GetItemAsync(id);
+            if (HandleResponseError(response, logger, "CustomTable", $"Id: '{id}'") is {} responseError)
+                return responseError;
+            return Ok(response.Value);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(CustomTableDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        public async Task<IActionResult> InsertAsync(CustomTableDto model)
+        {
+            var response = await service.InsertAsync(model);
+            if (HandleResponseError(response, logger, "CustomTable") is {} responseError)
+                return responseError;
+            return CreatedAtRoute(new { id = response.Value.Id }, response.Value);
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        public async Task<IActionResult> UpdateAsync(int id, CustomTableDto model)
+        {
+            if (!(model.Id == id))
+            {
+                return BadRequest($"Id: '{id}' must be equal to model.Id: '{model.Id}'");
+            }
+
+            var response = await service.UpdateAsync(id, model);
+            if (HandleResponseError(response, logger, "CustomTable", $"Id: '{id}'") is {} responseError)
+                return responseError;
+            return Ok(response.Value);
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var response = await service.DeleteItemAsync(id);
+            if (HandleResponseError(response, logger, "CustomTable", $"Id: '{id}'") is {} responseError)
+                return responseError;
+            return Ok(response.Value);
+        }
+    }
+}
