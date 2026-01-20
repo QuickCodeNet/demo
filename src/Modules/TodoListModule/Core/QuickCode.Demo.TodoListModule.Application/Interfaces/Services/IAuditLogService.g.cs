5247b2fb23dab285f2ec9b65d914a005706b1129
QuickCode Auto Commit <uzeyirapaydin@gmail.com>
2026-01-20 12:17:59 -0500

    QuickCode.Net Auto Commit: 1/20/2026 5:17:59 PM

---
+++ src/Modules/TodoListModule/Core/QuickCode.Demo.TodoListModule.Application/Interfaces/Services/IAuditLogService.g.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using QuickCode.Demo.Common.Models;
+using QuickCode.Demo.TodoListModule.Domain.Entities;
+using QuickCode.Demo.TodoListModule.Application.Interfaces.Repositories;
+using QuickCode.Demo.TodoListModule.Application.Dtos.AuditLog;
+
+namespace QuickCode.Demo.TodoListModule.Application.Services.AuditLog
+{
+    public partial interface IAuditLogService
+    {
+        Task<Response<AuditLogDto>> InsertAsync(AuditLogDto request);
+        Task<Response<bool>> DeleteAsync(AuditLogDto request);
+        Task<Response<bool>> UpdateAsync(Guid id, AuditLogDto request);
+        Task<Response<List<AuditLogDto>>> ListAsync(int? pageNumber, int? pageSize);
+        Task<Response<AuditLogDto>> GetItemAsync(Guid id);
+        Task<Response<bool>> DeleteItemAsync(Guid id);
+        Task<Response<int>> TotalItemCountAsync();
+    }
+}
